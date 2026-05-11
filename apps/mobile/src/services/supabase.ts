ea03ba47bf420f1cadb6d0bc6388bde44294c2f6
Lucas Guerra <lucasguerrawr2004@gmail.com>
2026-05-11 12:27:42 -0300

    feat: add async storage and secure store dependencies; update supabase client configuration

--- apps/mobile/src/services/supabase.ts
+++ apps/mobile/src/services/supabase.ts
@@ -1,15 +1,13 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 
-// O Expo lê automaticamente o que está no .env se tiver o prefixo EXPO_PUBLIC_
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    // Configurações para garantir que o login persista no telemóvel
+    persistSession: false, 
     autoRefreshToken: true,
-    persistSession: true,
     detectSessionInUrl: false,
   },
 });
